c89a398cd378e944faf848658784b2db9a91d053
David Nuon <david@davidnuon.com>
2016-04-11 12:47:06 -0700

    Got a little chat room going
    Using groups, you can direct where messages go.
    
    To test:
        - Run the app
        - Have different browsers connect to it.
        - Then in the console, use to connect via WebSockets
    
            socket = new WebSocket("ws://127.0.0.1:8000/chat/");
            socket.onmessage = function(e) {
                console.log(e.data);
            }
            socket.onopen = function() {
                socket.send("hello world");
            }
    
        - Use socket.send("MESSAGE STRING") to "chat" with the other
          browser. Make sure they're both listening.

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
 from channels.handler import AsgiHandler
+from channels import Group
+
+# Connected to websocket.connect
+def ws_add(message):
+    Group("chat").add(message.reply_channel)
+
+# Connected to websocket.disconnect
+def ws_disconnect(message):
+    Group("chat").discard(message.reply_channel)
 
 def http_consumer(message):
     # Make standard HTTP response - access ASGI path attribute directly
@@ -7,3 +16,8 @@ def http_consumer(message):
     # Encode that response into message format (ASGI)
     for chunk in AsgiHandler.encode_response(response):
         message.reply_channel.send(chunk)
+
+def ws_message(message):
+    Group("chat").send({
+        "text": "[user] %s" % message.content['text'],
+    })
--- chat/routing.py
+++ chat/routing.py
@@ -1,4 +1,8 @@
 from channels.routing import route
+
 channel_routing = [
     route("http.request", "chat.consumers.http_consumer"),
+    route("websocket.receive", "chat.consumers.ws_message"),
+    route("websocket.connect", "chat.consumers.ws_add"),
+    route("websocket.disconnect", "chat.consumers.ws_disconnect")
 ]
