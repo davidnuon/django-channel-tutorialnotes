0d2515ba236a9801a93ab39e7622e8d5cb35d556
David Nuon <david@davidnuon.com>
2016-04-11 15:42:19 -0700

    Added rooms

--- chat/routing.py
+++ chat/routing.py
@@ -1,8 +1,8 @@
 from channels.routing import route
+from chat.consumers import ws_connect, ws_message, ws_disconnect
 
 channel_routing = [
-    route("http.request", "chat.consumers.http_consumer"),
-    route("websocket.receive", "chat.consumers.ws_message"),
-    route("websocket.connect", "chat.consumers.ws_add"),
-    route("websocket.disconnect", "chat.consumers.ws_disconnect")
+    route("websocket.receive", ws_message),
+    route("websocket.connect", ws_connect),
+    route("websocket.disconnect", ws_disconnect)
 ]
